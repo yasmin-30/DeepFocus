71fcb715f25feafcf3d5303a8c1c24a356dd2ee0
yasmin-30 <161765927+yasmin-30@users.noreply.github.com>
2025-12-01 07:41:51 -0300

    Variável de término do Pomodoro

--- src/deepfocus/modelos/Pomodoro.java
+++ src/deepfocus/modelos/Pomodoro.java
@@ -5,6 +5,8 @@ import java.time.LocalDateTime;
 public class Pomodoro {
 
     private LocalDateTime inicio;
+    private LocalDateTime termino;
+    
     private int duracaoFoco;
     private int duracaoDescanso;
 
@@ -20,11 +22,13 @@ public class Pomodoro {
 
     // Getters
     public LocalDateTime getInicio() { return inicio; }
+    public LocalDateTime getTermino() { return termino; } 
     public int getDuracaoFoco() { return duracaoFoco; }
     public int getDuracaoDescanso() { return duracaoDescanso; }
 
     // Setters
     public void setInicio(LocalDateTime inicio) { this.inicio = inicio;}
+    public void setTermino(LocalDateTime termino) { this.termino = termino;} 
     public void setDuracaoFoco(int duracaoFoco) { this.duracaoFoco = duracaoFoco;}
     public void setDuracaoDescanso(int duracaoDescanso) { this.duracaoDescanso = duracaoDescanso;}
 }
